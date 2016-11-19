d3531c136bb1b4fba2a5e9e4674cfa009f058e61
Brian Hang <hangwithbrian@gmail.com>
2016-11-19 01:04:38 -0800

    Fixed some WakeUpEyes logic

--- JustinTime/stage/wakeup/wakeupstage.cpp
+++ JustinTime/stage/wakeup/wakeupstage.cpp
@@ -13,13 +13,13 @@ void WakeUpStage::draw(sf::RenderWindow &window) {
 }
 
 void WakeUpStage::onEvent(const sf::Event &e) {
+    // Open the eyes more every time a mouse button is pressed.
     if (e.type == sf::Event::MouseButtonPressed) {
         eyes.wakeUp(0.1f);
 
+        // If the eyes are fully open, go to another stage.
         if (eyes.isAwake()) {
             StageDirector::getInstance().setStage<MenuStage>();
-
-            return;
         }
     }
 }
