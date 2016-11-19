0c6ae7d7fceeaf4c23589f8331c45d21c57af2b1
Aviscer <Aviscer@DESKTOP-91TS07B>
2016-11-18 23:50:48 -0800

    Lecture

---
+++ JustinTime/lecturestage.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "stage.h"
+#include "wakeupeyes.h"
+
+class LectureStage : public Stage {
+
+
+	public: 
+		LectureStage();
+
+		void update(float deltaTime);
+		void draw(sf::RenderWindow &window);
+		void onEvent(const sf::Event &e);
+};
